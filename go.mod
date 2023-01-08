b7748785404d01ef5db4870b20343300c135ce99
Felix Geisendörfer <felix@felixge.de>
2023-01-08 13:15:30 +0100

    update main

--- go.mod
+++ go.mod
@@ -7,5 +7,8 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/tools v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"runtime/pprof"
+
+	"github.com/felixge/traceutils/pkg/encoding"
 )
 
 func main() {
 	cmd := Cmd{}
+	flag.StringVar(&cmd.CPUProfile, "cpuprofile", "", "write cpu profile to file")
 	flag.Parse()
+	cmd.File = flag.Arg(0)
 	if err := cmd.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
@@ -16,8 +22,46 @@ func main() {
 }
 
 type Cmd struct {
+	File       string
+	CPUProfile string
 }
 
 func (c *Cmd) Run() error {
-	return nil
+	cpuFile, err := os.Create(c.CPUProfile)
+	if err != nil {
+		return err
+	}
+	defer cpuFile.Close()
+
+	if err := pprof.StartCPUProfile(cpuFile); err != nil {
+		return err
+	}
+	defer pprof.StopCPUProfile()
+
+	file, err := os.Open(c.File)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	p, err := encoding.NewDecoder(file)
+	if err != nil {
+		return err
+	}
+
+	var n int
+	for {
+		var e encoding.Event
+		if err := p.Decode(&e); err == io.EOF {
+			fmt.Printf("%d events parsed\n", n)
+			return nil
+		} else if err != nil {
+			return err
+		}
+
+		if e.Type == encoding.EventString {
+			fmt.Printf("%s\n", e.Str)
+		}
+		n++
+	}
 }
