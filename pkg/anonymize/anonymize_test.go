cd5b8a211e313013ee727e9341d49be6a0b5e488
Felix Geisendörfer <felix@felixge.de>
2023-01-08 21:28:27 +0100

    Move stuff

--- pkg/anonymize/anonymize_test.go
+++ pkg/anonymize/anonymize_test.go
@@ -11,7 +11,39 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBytes(t *testing.T) {
+// TestAnonymizeTrace tests that we can anonymize an example trace.
+func TestAnonymizeTrace(t *testing.T) {
+	// Read the test trace.
+	inTrace, err := os.ReadFile(filepath.Join("..", "..", "testdata", "trace.bin"))
+	require.NoError(t, err)
+
+	// Anonymize the trace and write it to outTrace.
+	var outTrace bytes.Buffer
+	require.NoError(t, AnonymizeTrace(bytes.NewReader(inTrace), &outTrace))
+
+	// Create a decoder for the anonymized trace.
+	dec, err := encoding.NewDecoder(bytes.NewReader(outTrace.Bytes()))
+	require.NoError(t, err)
+
+	secretStrings := []string{"/Users/", "/felix.geisendoerfer/"}
+	for {
+		var ev encoding.Event
+		if err := dec.Decode(&ev); err != nil {
+			require.Equal(t, io.EOF, err)
+			break
+		}
+
+		if ev.Type == encoding.EventString {
+			// Check that the string does not contain any secret strings.
+			for _, s := range secretStrings {
+				require.NotContains(t, string(ev.Str), s)
+			}
+		}
+	}
+}
+
+// Test_anonymizeString tests the anonymizeString function.
+func Test_anonymizeString(t *testing.T) {
 	allowed := []string{"runtime", "encoding/json"}
 	tests := []struct {
 		name string
@@ -73,35 +105,5 @@ func TestBytes(t *testing.T) {
 				t.Errorf("got=%q want=%q", got, tt.want)
 			}
 		})
-	}
-}
-
-func TestAnonymizeTrace(t *testing.T) {
-	// Read the test trace.
-	inTrace, err := os.ReadFile(filepath.Join("..", "..", "testdata", "trace.bin"))
-	require.NoError(t, err)
-
-	// Anonymize the trace and write it to outTrace.
-	var outTrace bytes.Buffer
-	require.NoError(t, AnonymizeTrace(bytes.NewReader(inTrace), &outTrace))
-
-	// Create a decoder for the anonymized trace.
-	dec, err := encoding.NewDecoder(bytes.NewReader(outTrace.Bytes()))
-	require.NoError(t, err)
-
-	secretStrings := []string{"/Users/", "/felix.geisendoerfer/"}
-	for {
-		var ev encoding.Event
-		if err := dec.Decode(&ev); err != nil {
-			require.Equal(t, io.EOF, err)
-			break
-		}
-
-		if ev.Type == encoding.EventString {
-			// Check that the string does not contain any secret strings.
-			for _, s := range secretStrings {
-				require.NotContains(t, string(ev.Str), s)
-			}
-		}
 	}
 }
