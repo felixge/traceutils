558def54e414d83daf57f8be916a0da992273261
Felix Geisendörfer <felix@felixge.de>
2023-10-03 21:23:55 +0200

    stw: refactor test

--- pkg/stw/stw_test.go
+++ pkg/stw/stw_test.go
@@ -8,35 +8,68 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestEvents(t *testing.T) {
-	// Read the test trace.
-	inTrace, err := os.ReadFile(filepath.Join("..", "..", "testdata", "test-encoding-json.trace"))
-	require.NoError(t, err)
+	type testEvent struct {
+		Start    time.Duration
+		Duration time.Duration
+		Type     EventType
+	}
 
-	// Extract the STW events from the trace
-	events, err := Events(bytes.NewReader(inTrace))
-	require.NoError(t, err)
+	tests := []struct {
+		GoVersion   string
+		EventCount  int
+		CheckEvents map[int]testEvent
+	}{
+		{
+			GoVersion:  "1.19",
+			EventCount: 42,
+			CheckEvents: map[int]testEvent{
+				0: {
+					Start:    2323248,
+					Duration: 18784,
+					Type:     SweepTermination,
+				},
+				41: {
+					Start:    504956960,
+					Duration: 89376,
+					Type:     MarkTermination,
+				},
+			},
+		},
+	}
 
-	// Assert the number of STW events in the trace.
-	require.Equal(t, 42, len(events))
+	for _, test := range tests {
+		t.Run(test.GoVersion, func(t *testing.T) {
+			// Read the test trace.
+			inTrace, err := os.ReadFile(filepath.Join("..", "..", "testdata", test.GoVersion, "test-encoding-json.trace"))
+			require.NoError(t, err)
 
-	// Sort events by start time in ascending order
-	sort.Slice(events, func(i, j int) bool {
-		return events[i].Start < events[j].Start
-	})
+			// Extract the STW events from the trace
+			events, err := Events(bytes.NewReader(inTrace))
+			require.NoError(t, err)
 
-	// Validate first event
-	first := events[0]
-	require.Equal(t, time.Duration(2323248), first.Start)
-	require.Equal(t, time.Duration(18784), first.Duration())
-	require.Equal(t, SweepTermination, first.Type)
+			// Assert the number of STW events in the trace.
+			require.Equal(t, test.EventCount, len(events))
 
-	// Validate last event
-	last := events[len(events)-1]
-	require.Equal(t, time.Duration(504956960), last.Start)
-	require.Equal(t, time.Duration(89376), last.Duration())
-	require.Equal(t, MarkTermination, last.Type)
+			// Sort events by start time in ascending order
+			sort.Slice(events, func(i, j int) bool {
+				return events[i].Start < events[j].Start
+			})
+
+			// Check some of the STW events in the trace.
+			for i, e := range events {
+				want, ok := test.CheckEvents[i]
+				if !ok {
+					continue
+				}
+				assert.Equal(t, want.Start, e.Start)
+				assert.Equal(t, want.Duration, e.Duration())
+				assert.Equal(t, want.Type, e.Type)
+			}
+		})
+	}
 }
