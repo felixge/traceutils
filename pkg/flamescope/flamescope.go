cd8818205f37621821801b42f414e5c7f8892dcb
dongbiao <dong.biao@qq.com>
2025-10-30 08:29:25 +0800

    traceutils flamescope supports the latest trace file format

--- pkg/flamescope/flamescope.go
+++ pkg/flamescope/flamescope.go
@@ -1,100 +1,83 @@
 package flamescope
 
 import (
-	"errors"
 	"fmt"
 	"io"
+	"math"
 
-	"github.com/felixge/traceutils/pkg/encoding"
+	"golang.org/x/exp/trace"
 )
 
 // FlameScope reads a trace from r and writes it to w in a format [1] that can
 // be visualized by the flamescope tool.
 //
 // [1] https://github.com/Netflix/flamescope/blob/be26595f1395c32eef71c88a06cb9c8c87f270c2/app/perf/regexp.py
-func FlameScope(r io.Reader, w io.Writer) (err error) {
-	var (
-		dec         = encoding.NewDecoder(r)        // event decoder
-		ev          encoding.Event                  // current event
-		ticksPerSec int64                           // ticks per second
-		minTS       uint64                          // minimum timestamp
-		cpuSamples  []cpuSample                     // cpu samples
-		stacks      = make(map[uint64][]stackFrame) // stack id : stack frames
-		strings     = make(map[uint64]string)       // string id : string
-	)
-	// Read all raw events
-	for {
-		// Read the next event
-		err := dec.Decode(&ev)
-		if err != nil {
-			if err == io.EOF {
-				// We're done
-				break
-			}
-			// Failed to decode event
-			return err
-		}
-
-		switch ev.Type {
-		// CPU profiling sample [timestamp, real timestamp, real P id (-1 when absent), goroutine id, stack]
-		case encoding.EventCPUSample:
-			sample := cpuSample{Timestamp: ev.Args[1], StackID: ev.Args[4]}
-			cpuSamples = append(cpuSamples, sample)
-			if minTS == 0 || sample.Timestamp < minTS {
-				minTS = sample.Timestamp
-			}
-		// stack [stack id, number of PCs, array of {PC, func string ID, file string ID, line}]
-		case encoding.EventStack:
-			stackID, numFrames := ev.Args[0], ev.Args[1]
-			frames := make([]stackFrame, numFrames)
-			args := ev.Args[2:]
-			for i := 0; i < len(frames); i++ {
-				frames[i].PC = args[i*4+0]
-				frames[i].FuncID = args[i*4+1]
-				frames[i].FileID = args[i*4+2]
-				frames[i].Line = args[i*4+3]
-			}
-			stacks[stackID] = frames
-		// string dictionary entry [ID, length, string]
-		case encoding.EventString:
-			strings[ev.Args[0]] = string(ev.Str)
-		case encoding.EventFrequency:
-			// ticksPerSec is used to convert ticks to nanoseconds
-			ticksPerSec = int64(ev.Args[0])
-		}
+func FlameScope(r io.Reader, w io.Writer) error {
+	minTs, cpuSamples, err := getCpuSamples(r)
+	if err != nil {
+		return err
 	}
-
-	if ticksPerSec <= 0 {
-		return errors.New("missing or bad EventFrequency event")
-	}
-
 	// Convert cpu samples to perf script format used by flamescope
-	for _, cs := range cpuSamples {
-		// Convert ticks to seconds
-		ts := float64(cs.Timestamp-minTS) / float64(ticksPerSec)
+	for i := 0; i < len(cpuSamples); i++ {
 		// Write the stack trace header with timestamp
-		fmt.Fprintf(w, "go 0 [0] %f: cpu-clock:\n", ts)
+		_, _ = fmt.Fprintf(w, "go 0 [0] %f: cpu-clock:\n", float64(cpuSamples[i].Timestamp-minTs)/1e9)
 		// Write out the individual stack frames
-		stack := stacks[cs.StackID]
-		for _, sf := range stack {
-			fn := strings[sf.FuncID]
-			fmt.Fprintf(w, "\t%x %s (go)\n", sf.PC, fn)
+		frames := cpuSamples[i].Frames
+		for j := 0; j < len(frames); j++ {
+			_, _ = fmt.Fprintf(w, "\t%x %s (go)\n", frames[j].PC, frames[j].Func)
 		}
-		fmt.Fprintf(w, "\n")
+		_, _ = fmt.Fprintf(w, "\n")
 	}
 	return nil
 }
 
 // cpuSample represents a single cpu sample.
 type cpuSample struct {
-	Timestamp uint64
-	StackID   uint64
+	Timestamp int64
+	Frames    []stackFrame
 }
 
 // stackFrame represents a single stack frame.
 type stackFrame struct {
-	PC     uint64
-	FuncID uint64
-	FileID uint64
-	Line   uint64
+	PC   uint64
+	Func string
+}
+
+func getCpuSamples(f io.Reader) (int64, []cpuSample, error) {
+	r, err := trace.NewReader(f)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	minTs := int64(math.MaxInt64)
+	cpuSamples := make([]cpuSample, 0, 100)
+	var ts int64
+
+	for {
+		ev, err := r.ReadEvent()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return 0, nil, err
+		}
+
+		if ev.Kind() == trace.EventStackSample {
+			if stk := ev.Stack(); stk != trace.NoStack {
+				ts = int64(ev.Time())
+				if ts < minTs {
+					minTs = ts
+				}
+				frames := make([]stackFrame, 0, 30)
+				for f := range stk.Frames() {
+					frames = append(frames, stackFrame{PC: f.PC, Func: f.Func})
+				}
+				cpuSamples = append(cpuSamples, cpuSample{Timestamp: ts, Frames: frames})
+			}
+		}
+	}
+	if len(cpuSamples) <= 0 {
+		return 0, nil, fmt.Errorf("not found cpu sample")
+	}
+	return minTs, cpuSamples, nil
 }
