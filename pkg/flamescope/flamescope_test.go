73c5fb1b8ab4dbe78711a3195c673c58208aa5e1
Felix Geisendörfer <felix@felixge.de>
2025-11-02 16:06:59 +0100

    chore: add flamescope test for go1.25

--- pkg/flamescope/flamescope_test.go
+++ pkg/flamescope/flamescope_test.go
@@ -11,14 +11,32 @@ import (
 )
 
 func TestFlameScope(t *testing.T) {
-	// Read the test trace.
-	inTrace, err := os.ReadFile(filepath.Join("..", "..", "testdata", "1.19", "test-encoding-json.trace"))
-	require.NoError(t, err)
+	tests := []struct {
+		name    string
+		version string
+	}{
+		{
+			name:    "go1.19",
+			version: "1.19",
+		},
+		{
+			name:    "go1.25",
+			version: "1.25",
+		},
+	}
 
-	// Extract the STW events from the trace
-	var out bytes.Buffer
-	require.NoError(t, FlameScope(bytes.NewReader(inTrace), &out))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Read the test trace.
+			inTrace, err := os.ReadFile(filepath.Join("..", "..", "testdata", tt.version, "test-encoding-json.trace"))
+			require.NoError(t, err)
 
-	// Compare the output to the expected output.
-	snaps.MatchSnapshot(t, out.String())
+			// Extract the STW events from the trace
+			var out bytes.Buffer
+			require.NoError(t, FlameScope(bytes.NewReader(inTrace), &out))
+
+			// Compare the output to the expected output.
+			snaps.MatchSnapshot(t, out.String())
+		})
+	}
 }
